00620e90caa3c317c1ec416adefaa0ef255ddc36
Sanghee Kim <sangheestyle@gmail.com>
2018-12-01 15:46:48 +0200

    error handling for not exist

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,17 @@
 import rp from "request-promise";
 
 export async function getUser(userId: number) {
-  const { firstName, lastName, age } = await rp({
-    uri: `http://localhost:3000/api/users/${userId}`,
-    json: true,
-  });
+  try {
+    const response = await rp({
+      uri: `http://localhost:3000/api/users/${userId}`,
+      json: true,
+    });
 
-  return {
-    fullName: `${firstName} ${lastName}`,
-    firstName,
-    lastName,
-    age
+    return {
+      ...response,
+      fullName: `${response.firstName} ${response.lastName}`,
+    };
+  } catch (error) {
+    return {};
   }
 }
--- test/api.test.ts
+++ test/api.test.ts
@@ -4,7 +4,6 @@ import { getUser } from "../src/api";
 describe("GET /api/users/:id", () => {
   it("return user object if it is exist", async() => {
     nock("http://localhost:3000")
-      .persist()
       .get("/api/users/1")
       .reply(200, {
         firstName: "ari",
@@ -20,5 +19,15 @@ describe("GET /api/users/:id", () => {
         lastName: "kim",
         age: 1,
       });
+  });
+
+  it("return empty plain object if id is not exist", async() => {
+    nock("http://localhost:3000")
+      .get("/api/users/100")
+      .reply(404, {});
+
+    const result = await getUser(100);
+    expect(result)
+      .toEqual({});
   });
 });
